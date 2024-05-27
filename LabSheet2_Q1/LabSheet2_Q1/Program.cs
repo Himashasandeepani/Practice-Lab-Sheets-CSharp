caf35dc1b61337853e8c01de714ba20d4b38ab7c
Himashasandeepani <135612092+Himashasandeepani@users.noreply.github.com>
2024-05-27 16:20:55 +0530

    Lab Sheet 2

---
+++ LabSheet2_Q1/LabSheet2_Q1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSheet2_Q1
+{
+    //class- Book
+    public class Book
+    {
+        //variables - get, set
+        public string title { get; set; }
+        public string author { get; set; }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //object
+            Book book1 = new Book();
+
+            //called
+            book1.title = "madol Duwa";
+            book1.author = "Mr.Martin Wikramasinghe";
+
+            //output
+            Console.WriteLine("Title : " + book1.title);
+            Console.WriteLine("Autor :" + book1.author);
+            Console.ReadLine();
+        }
+    }
+}
+
+/*
+Output- 
+
+Title : madol Duwa
+Autor :Mr.Martin Wikramasinghe
+*/
