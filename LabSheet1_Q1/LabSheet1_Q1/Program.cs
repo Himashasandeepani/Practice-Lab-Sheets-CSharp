9895bda1da7d31793719ce7ee480d1b6c4582361
Himashasandeepani <135612092+Himashasandeepani@users.noreply.github.com>
2024-05-27 16:14:17 +0530

    Lab Sheet 1

---
+++ LabSheet1_Q1/LabSheet1_Q1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSheet1_Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //User inputs
+            Console.Write("Enter the rectangle length : ");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the rectangle width : ");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            //calculate area
+            double area = length * width;
+
+            //output
+            Console.WriteLine("\nArea : " +area);
+            Console.ReadLine();
+            
+        }
+    }
+}
+
+/*
+Output-
+
+Enter the rectangle length : 5
+Enter the rectangle width : 3.5
+
+Area : 17.5
+*/
